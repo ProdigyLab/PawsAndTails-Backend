a8f49c1a4a3b127b82ec6b85a26e6117aa4c1bd2
sudiptaadhikaryjoy <sudiptaadhikaryjoy@gmail.com>
2024-10-24 16:46:58 +0600

    fix: folder structure change and package installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configService from './database/dbconfigue';
-import { LoginModule } from './login/login.module';
-import { PetInfoModule } from './petInfo/petInfo.module';
-import { ProductModule } from './products/products.module';
-import { UsersModule } from './users/users.module';
+import configService from '../database/dbconfigue';
+import { LoginModule } from '../login/login.module';
+import { PetInfoModule } from '../petInfo/petInfo.module';
+import { ProductModule } from '../products/products.module';
+import { UsersModule } from '../users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
